b2cf7c0c8704c6e3cd7364714130e0400d4c0414
naneko <54571022+naneko@users.noreply.github.com>
2021-04-14 12:05:57 -0400

    Fix decode_token

--- polyphony/helpers/decode_token.py
+++ polyphony/helpers/decode_token.py
@@ -3,6 +3,7 @@ import logging
 
 log = logging.getLogger(__name__)
 
+
 def decode_token(token: str):
     token = token.split(".")
-    return base64.b64decode(token[0])
+    return int(base64.b64decode(token[0]))
